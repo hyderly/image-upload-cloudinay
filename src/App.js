ab434b02f4420c82425a266664e53e1f2b258fb6
haider <haidertiger10@gmail.com>
2021-09-07 13:25:46 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import axios from 'axios';
 
 function App() {
+
+
+  const [img, setImg] = useState(null);
+
+  const uploadFile = (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', 'vmvrpojg');
+
+    const cloudinaryName = 'hyderly'
+
+    try{
+      axios.post(`https://api.cloudinary.com/v1_1/${cloudinaryName}/upload`, formData)
+      .then((response) => setImg(response.data.secure_url));
+      
+    }catch(err){
+      console.log(err);
+    }
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" onChange={uploadFile}/>
+      {
+        img && <img src={img} alt="img" style={{width: "200px"}}/>
+      }
+      
     </div>
   );
 }
